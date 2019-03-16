e792dfdf862eead195321e368929ca5d6059d8e9
lazyartist <uuux3@yahoo.co.kr>
2019-03-17 01:48:45 +0900

    AStar basic

---
+++ 0701 AStar Basic/Assets/Scripts/AStarWithBlock.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AStarWithBlock : MonoBehaviour
+{
+    public NodeContainer NodeContainer;
+
+    public bool AutoStartToSearch = false;
+
+    private Node _curNode;
+
+    private void Start()
+    {
+        NodeContainer.CreateNodes();
+
+        _curNode = NodeContainer.StartNode;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit[] hits = Physics.RaycastAll(ray);
+            foreach (RaycastHit hit in hits)
+            {
+                if (hit.collider.tag == "Next" && _curNode != null)
+                {
+                    SearchPath(_curNode);
+                }
+            }
+        }
+
+        if (AutoStartToSearch && _curNode != null)
+        {
+            SearchPath(_curNode);
+        }
+    }
+
+    void SearchPath(Node curNode)
+    {
+        U.d("SearchPath", curNode);
+
+        curNode.Closed = true;
+
+        Collider[] colliders = NodeContainer.GetAroundBlocks(curNode);
+
+        if (colliders.Length == 0) return;
+
+        Node minFitnessNode = null;
+        //Node minFitnessNode = colliders[0].GetComponent<Node>();
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            Node node = colliders[i].gameObject.GetComponent<Node>();
+
+            if (node.IsBlock) continue;
+
+            if (node != null)
+            {
+                if (curNode == node || node.Closed == true) continue;
+
+                var disFromCur = Vector3.Distance(curNode.transform.position, node.transform.position);
+                var disToEnd = Vector3.Distance(NodeContainer.EndNode.transform.position, node.transform.position);
+
+                node.goal = curNode.fitness + disFromCur;
+                node.heuristic = disToEnd;
+                node.fitness = node.goal + node.heuristic;
+
+                if (minFitnessNode == null || minFitnessNode.fitness > node.fitness)
+                {
+                    minFitnessNode = node;
+                }
+
+                node.SetColor(Color.gray);
+            }
+        }
+
+        if(minFitnessNode == curNode)
+        {
+            // 사방이 막혔다. 종료
+            return;
+        }
+
+        minFitnessNode.Closed = true;
+        minFitnessNode.SetColor(Color.blue);
+        _curNode = minFitnessNode;
+
+        if (_curNode == NodeContainer.EndNode)
+        {
+            _curNode.SetColor(Color.green);
+            _curNode = null;
+            return;
+        }
+    }
+}
---
+++ 0701 AStar Basic/Assets/Scripts/Node.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+    public Renderer Renderer;
+
+    public bool Closed = false;
+    public bool IsBlock = false;
+
+    public float fitness; // 적절성 : goal + heuristic
+    public float goal; // 목표 : 시작에서 현재까지 거리
+    public float heuristic; // 추정치 : 종료에서 현재까지  거리
+
+    private Color _color;
+
+    private void Start()
+    {
+        _color = Renderer.material.GetColor("_Color");
+    }
+
+    public void SetColor(Color color)
+    {
+        _color = color;
+        Renderer.material.SetColor("_Color", color);
+    }
+
+    public void Click()
+    {
+        U.d(name, fitness, goal, heuristic);
+        StartCoroutine(Coroutine_Color());
+    }
+
+    IEnumerator Coroutine_Color()
+    {
+        Renderer.material.SetColor("_Color", Color.red);
+
+        yield return new WaitForSeconds(1.0f);
+
+        Renderer.material.SetColor("_Color", _color);
+    }
+
+}
